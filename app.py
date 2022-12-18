4405cdd336d2eadbadf3a3321ed91c9ff6a17171
Saravanan <87254556+Saravanan-SD@users.noreply.github.com>
2022-12-18 13:21:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,8 @@ model = joblib.load('clf.pkl')
 
 st.set_page_config(page_title="Accident Severity Prediction App",
                   layout="wide")
-add_selectbox = st.sidebar.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
+# Object notation
+st.sidebar.[element_name]
 
 #creating option list for dropdown menu
 options_day = ['Sunday', "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
